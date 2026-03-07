58e9ff2e4a7ccf87d3f0bdbab9ac1b532943bb6f
autumn0b <nel.malachi@proton.me>
2026-03-07 03:03:23 -0600

    fix

--- nvim/nvim/lua/colors/kangawa_paper.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"thesimonho/kanagawa-paper.nvim",
-	enabled = true,
-	config = function()
-		require("kanagawa-paper").setup({
-			transparent = true
-		})
-	end
-}
--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,73 +1,111 @@
 # config files
-echo "copying config files:"
+printf "copying config files:\n"
 
+# zsh
 cp ~/.zshrc                       ~/sync/linux_config/
-echo "  zsh"
+printf "  zsh cp\n\n"
 
-if [ -d "~/sync/linux_config/foot/" ]; then
+
+# foot
+if [ -d "$HOME/sync/linux_config/foot/" ]; then
 	rm -r ~/sync/linux_config/foot/
+	printf "  foot rm\n"
 fi
 cp -r ~/.config/foot/             ~/sync/linux_config/
-echo "  foot"
+printf "  foot cp\n\n"
 
-if [ -d "~/sync/linux_config/hypr/" ]; then
+
+# ghostty
+if [ -d "$HOME/sync/linux_config/ghostty/" ]; then
+	rm -r ~/sync/linux_config/ghostty/
+	printf "  ghostty rm\n"
+fi
+cp -r ~/.config/ghostty/             ~/sync/linux_config/
+printf "  ghostty cp\n\n"
+
+
+# hypr
+if [ -d "$HOME/sync/linux_config/hypr/" ]; then
 	rm -r ~/sync/linux_config/hypr/
+	printf "  hypr rm\n"
 fi
 cp -r ~/.config/hypr/             ~/sync/linux_config/
-echo "  hypr"
+printf "  hypr cp\n\n"
 
-if [ -d " ~/sync/linux_config/nvim/" ]; then
+
+# nvim
+if [ -d "$HOME/sync/linux_config/nvim/" ]; then
 	rm -r ~/sync/linux_config/nvim/
+	printf "  nvim rm\n"
 fi
-cp -r ~/.config/nvim/             ~/sync/linux_config/nvim/
-echo "  nvim"
+cp -r ~/.config/nvim/             ~/sync/linux_config/
+printf "  nvim cp\n\n"
 
-if [ -d " ~/sync/linux_config/rofi/" ]; then
+
+# rofi
+if [ -d "$HOME/sync/linux_config/rofi/" ]; then
 	rm -r ~/sync/linux_config/rofi/
+	printf "  rofi rm\n"
 fi
 cp -r ~/.config/rofi/             ~/sync/linux_config/
-echo "  rofi"
+printf "  rofi cp\n\n"
 
-if [ -d " ~/sync/linux_config/waybar/" ]; then
+
+# waybar
+if [ -d "$HOME/sync/linux_config/waybar/" ]; then
 	rm -r ~/sync/linux_config/waybar/
+	printf "  waybar rm\n"
 fi
 cp -r ~/.config/waybar/           ~/sync/linux_config/
-echo "  waybar"
+printf "  waybar cp\n\n"
 
-if [ -d " ~/sync/linux_config/yazi/" ]; then
+
+# yazi
+if [ -d "$HOME/sync/linux_config/yazi/" ]; then
 	rm -r ~/sync/linux_config/yazi/
+	printf "  yazi rm\n"
 fi
 cp -r ~/.config/yazi/             ~/sync/linux_config/
-echo "  yazi"
+printf "  yazi cp\n\n"
 
-if [ -d " ~/sync/linux_config/zellij/" ]; then
+
+# zellij
+if [ -d "$HOME/sync/linux_config/zellij/" ]; then
 	rm -r ~/sync/linux_config/zellij/
+	printf "  zellij rm\n"
 fi
 cp -r ~/.config/zellij/           ~/sync/linux_config/
-echo "  zellij"
+printf "  zellij cp\n\n"
 
 
-# other
-if [ -d " ~/sync/linux_config/factorio/" ]; then
+# factorio
+if [ -d "$HOME/sync/linux_config/factorio/" ]; then
 	rm -r ~/sync/linux_config/factorio/
+	printf "  factorio rm\n"
 fi
 mkdir -p ~/sync/linux_config/factorio/
 cp ~/.factorio/config/config.ini  ~/sync/linux_config/factorio/
-echo "  factorio"
+printf "  factorio cp\n\n"
 
+
+# reaper
 cp ~/.config/REAPER/reaper-kb.ini ~/sync/linux_config/
-echo "  reaper"
+printf "  reaper cp\n\n"
 
-# requires sudo
-if [ -d " ~/sync/linux_config/keyd/" ]; then
+
+# keyd
+if [ -d "$HOME/sync/linux_config/keyd/" ]; then
 	sudo rm -rf ~/sync/linux_config/keyd/
+	printf "  keyd rm\n"
 fi
 cp -r /etc/keyd/                 ~/sync/linux_config/
-echo "  keyd"
+printf "  keyd cp\n\n"
 
+
+# nixos
 if [ -d "/etc/nixos/" ]; then
 	sudo cp -r /etc/nixos/configuration.nix ~/sync/linux_config/
-	echo "  nixos"
+	printf "  nixos cp\n\n"
 fi
 
-echo "complete"
+echo "complete\n"
