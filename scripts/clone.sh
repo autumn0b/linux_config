7fd51c96422e8fbe5f0a9590f22808a90fb8af9a
autumn0b <nel.malachi@proton.me>
2026-03-08 03:16:38 -0500

    fix to clone.sh

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -1,14 +1,14 @@
 mkdir -p ~/.config/
 
-cp -r ~/sync/linux_config/hypr/ ~/.config/hypr/
-cp -r ~/sync/linux_config/waybar/ ~/.config/waybar/
-cp -r ~/sync/linux_config/rofi/ ~/.config/rofi/
+cp -r ~/sync/linux_config/hypr/ ~/.config/
+cp -r ~/sync/linux_config/waybar/ ~/.config/
+cp -r ~/sync/linux_config/rofi/ ~/.config/
 
-cp -r ~/sync/linux_config/ghostty/ ~/.config/ghostty/
-cp -r ~/sync/linux_config/nvim/ ~/.config/nvim/
+cp -r ~/sync/linux_config/ghostty/ ~/.config/
+cp -r ~/sync/linux_config/nvim/ ~/.config/
 cp ~/sync/linux_config/.zshrc ~/.zshrc
 
 printf "Warning: { nix, reaper, and factorio } config files are not automatically copied\n"
 
 sudo mkdir -p /etc/keyd/
-sudo cp -r ~/sync/linux_config/keyd/ /etc/keyd/
+sudo cp -r ~/sync/linux_config/keyd/ /etc/
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,2 +1,5 @@
+sudo systemctl enable keyd --now
+sudo keyd reload
+
 git config --global user.email "nel.malachi@proton.me"
 git config --global user.name "autumn0b"
